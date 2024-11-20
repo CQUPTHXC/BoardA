9c91a80d1458b8297960269f98dd1cc1dd4b55db
qingmeijiupiao <2368357055@qq.com>
2024-11-20 11:00:02 +0800

    添加模块

--- 复用模块/CAN模块/ESP_CAN.hpp
+++ 复用模块/CAN模块/ESP_CAN.hpp
@@ -1,196 +1,11 @@
-#ifndef DBUS_HPP
-#define DBUS_HPP
-#include <Arduino.h>
-
-enum DBUS_CHANNEL {
-  LEFT_X = 0,
-  LEFT_Y = 1,
-  RIGHT_X = 2,
-  RIGHT_Y = 3,
-  s1=4,
-  s2=5,
-  MOUSE_X=6,
-  MOUSE_Y=7,
-  MOUSE_Z=8,
-  MOUSE_LEFT_BUTTON=9,
-  MOUSE_RIGHT_BUTTON=10,
-  BUTTON_W=11,
-  BUTTON_S=12,
-  BUTTON_A=13,
-  BUTTON_D=14,
-  BUTTON_Q=15,
-  BUTTON_E=16,
-  BUTTON_SHIFT=17,
-  BUTTON_CTRL=18
-};
-
-class DBUS {
-  public:
-  DBUS(const DBUS &obj) = delete;
-  DBUS &operator=(const DBUS &obj) = delete;
-	DBUS(uint8_t pin, HardwareSerial *serial) {
-		_pin = pin;
-		_serial = serial;
-	};
-	DBUS(uint8_t pin) {
-		_pin = pin;
-		_serial = &Serial2;
-	};
-	DBUS() {
-		_serial = &Serial2;
-		_pin = 6;
-	};
-
-	void setup() {
-		pinMode(_pin, INPUT);
-		_serial->begin(100000, SERIAL_8E2, _pin, 38);
-        _serial->setRxBufferSize(18*20);
-		memset(raw_data, 0, sizeof(raw_data));
-		memset(channel_data, 0, sizeof(channel_data));
-		xTaskCreate(loop_task, "loop_task", 4096, this, 2, NULL);
-	};
-	uint16_t get_left_y(){
-    return channel_data[3];
-  }
-
-  uint16_t get_right_y(){
-    return channel_data[1];
-  }
-
-  uint16_t get_left_x(){
-    return channel_data[2];
-  }
-
-  uint16_t get_right_x(){
-    return channel_data[0];
-  }
-  float get_channel_data(DBUS_CHANNEL channel){
-    switch (channel)
-    {
-    case LEFT_X:
-      return float(channel_data[2]-1024)/1320.f;
-    case LEFT_Y:
-      return float(channel_data[3]-1024)/1320.f;
-    case RIGHT_X:
-      return float(channel_data[0]-1024)/1320.f;
-    case RIGHT_Y:
-      return float(channel_data[1]-1024)/1320.f;
-    case s1:
-      return this->S1;
-    case s2:
-      return this->S2;
-    case MOUSE_X:
-      return this->mouse_x/32767.f;
-    case MOUSE_Y:
-      return this->mouse_y/32767.f;
-    case MOUSE_Z:
-      return this->mouse_z/32767.f;
-    case MOUSE_LEFT_BUTTON:
-      return this->mouse_left_button;
-    case MOUSE_RIGHT_BUTTON:
-      return this->mouse_right_button;
-    case BUTTON_W:
-      return this->button_W;
-    case BUTTON_S: 
-      return this->button_S;
-    case BUTTON_A:
-      return this->button_A;
-    case BUTTON_D:
-      return this->button_D;
-    case BUTTON_Q:
-      return this->button_Q;
-    case BUTTON_E:
-      return this->button_E;
-    case BUTTON_SHIFT:
-      return this->button_Shift;
-    case BUTTON_CTRL:
-      return this->button_Ctrl;
-    default:
-      break;
-    }
-  }
-  protected:
-  static void loop_task(void *p) {
-    DBUS *obj = (DBUS *)p;
-    auto check_S1_data=[](uint8_t s1byte){
-        int value=((s1byte >> 4) & 0x000C) >> 2;
-        return value<4&&value>0;
-    };
-    auto check_S2_data=[](uint8_t s2byte){
-        int value=(s2byte >> 4) & 0x0003;
-        return value<4&&value>0;
-    };
-
-
-    //检查S1和S2的值来对齐字节流
-    auto data_check =[&](){
-        if (obj->_serial->available() < 36) {
-            delay(1);
-        }
-        while(!check_S1_data(obj->_serial->read())){
-            if(check_S2_data(obj->_serial->peek())){
-                for(int i=0;i<13;i++){
-                    obj->_serial->read();
-                }
-                break;
-            }
-            obj->_serial->read();
-        }
-    };
-        
-		data_check();
-		while (true) {
-      if (obj->_serial->available() < 18) {
-				delay(1);
-				continue;
-			}
-      obj->_serial->readBytes(obj->raw_data, 18);
-      obj->channel_data[0]=((int16_t)obj->raw_data[0] | ((int16_t)obj->raw_data[1] << 8)) & 0x07FF;
-      obj->channel_data[1]=(((int16_t)obj->raw_data[1] >> 3) | ((int16_t)obj->raw_data[2] << 5)) & 0x07FF;
-
-      obj->channel_data[2]= (((int16_t)obj->raw_data[2] >> 6) | ((int16_t)obj->raw_data[3] << 2) |((int16_t)obj->raw_data[4] << 10)) & 0x07FF;
-      obj->channel_data[3]= (((int16_t)obj->raw_data[4] >> 1) | ((int16_t)obj->raw_data[5]<<7)) & 0x07FF;
-
-      obj->mouse_x=*((int16_t*)(obj->raw_data+6));
-      obj->mouse_y=*((int16_t*)(obj->raw_data+8));
-      obj->mouse_z=*((int16_t*)(obj->raw_data+10));
-      obj->mouse_left_button=*(bool*)(obj->raw_data+12);
-      obj->mouse_right_button=*(bool*)(obj->raw_data+13);
-      uint8_t button_byte= obj->raw_data[14];
-      obj->button_W=button_byte & 0x01;
-      obj->button_S=button_byte & 0x02;
-      obj->button_A=button_byte & 0x04;
-      obj->button_D=button_byte & 0x08;
-      obj->button_Q=button_byte & 0x10;
-      obj->button_E=button_byte & 0x20;
-      obj->button_Shift=button_byte & 0x40;
-      obj->button_Ctrl=button_byte & 0x80;
-    }
-	}
-	bool is_first = true;
-	uint8_t _pin;
-	HardwareSerial *_serial;
-	uint8_t raw_data[18];
-	uint16_t channel_data[4];
-  uint8_t S1=0;
-  uint8_t S2=0;
-  int16_t mouse_x=0;
-  int16_t mouse_y=0;
-  int16_t mouse_z=0;
-  bool mouse_left_button=0;
-  bool mouse_right_button=0;
-  bool button_W=0;
-  bool button_S=0;
-  bool button_A=0;
-  bool button_D=0;
-  bool button_Q=0;
-  bool button_E=0;
-  bool button_Shift=0;
-  bool button_Ctrl=0;
-	uint8_t flag;
-};
-
-#endif
+/*
+ * @Description: 对twai库的二次封装,实现CAN回调函数,默认1Mbps波特率
+ * @Author: qingmeijiupiao
+ * @Date: 2024-04-13 21:00:21
+ * @LastEditTime: 2024-11-20 10:31:32
+ * @LastEditors: qingmeijiupiao
+ * @rely:
+*/
 /*
                                               .=%@#=.                                               
                                             -*@@@@@@@#=.                                            
@@ -262,3 +77,73 @@ class DBUS {
                     ####                                                                            
                       K                                                                             
 */
+#ifndef ESP_CAN_HPP
+#define ESP_CAN_HPP
+#include <Arduino.h>
+#include "driver/twai.h" //can驱动,esp32sdk自带
+
+/*↓↓↓本文件的类和函数↓↓↓*/
+
+//初始化CAN
+void  can_setup(uint8_t TX_PIN=8, uint8_t RX_PIN=18);//这里默认是HXC开发板A的CAN芯片引脚
+
+//添加用户自定义的can消息接收函数，用于处理非电机的数据,addr：要处理的消息的地址，func：回调函数
+void  add_user_can_func(int addr,std::function<void(twai_message_t* can_message)> func);
+
+//全局CAN反馈任务,无需手动创建
+void  can_feedback_update_task(void* n);
+
+/*↑↑↑本文件的类和函数↑↑↑*/
+
+
+
+//初始化CAN
+void  can_setup(uint8_t TX_PIN, uint8_t RX_PIN){
+    //TX是指CAN收发芯片的TX,RX同理
+    //检测TWAI驱动是否已经安装
+    twai_status_info_t now_twai_status;
+    auto status = twai_get_status_info(&now_twai_status);
+    if(status != ESP_ERR_INVALID_STATE){//如果驱动重复安装
+        return;
+    }
+
+
+    //总线速率,1Mbps
+    static const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_1MBITS();
+    //滤波器设置,接受所有地址的数据
+    static const twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+    //总线配置
+    static const twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(gpio_num_t(TX_PIN), gpio_num_t(RX_PIN), TWAI_MODE_NO_ACK);
+
+    //传入驱动配置信息
+    twai_driver_install(&g_config, &t_config, &f_config);
+    //CAN驱动启动
+    twai_start();
+    //创建任务
+    xTaskCreate(can_feedback_update_task,"can_fb",4096,nullptr,5,nullptr);//can反馈任务
+
+};
+
+//CAN反馈函数
+using can_feedback_func= std::function<void(twai_message_t* can_message)>;
+
+std::map<int,can_feedback_func> func_map;//can回调map
+
+//添加用户自定义的can消息接收函数，用于处理非电机的数据,addr：要处理的消息的地址
+void add_user_can_func(int addr,std::function<void(twai_message_t* can_message)> func){
+    func_map[addr]=func;
+};
+
+//接收CAN总线上的数据的任务函数
+void can_feedback_update_task(void* n){
+    twai_message_t rx_message;
+    while (1){
+        //接收CAN上数据
+        ESP_ERROR_CHECK(twai_receive(&rx_message, portMAX_DELAY));
+        //查看是否为需要的can消息地址，如果是就调用函数
+        if(func_map.find(rx_message.identifier)!=func_map.end()){
+            func_map[rx_message.identifier](&rx_message);
+        }
+    }
+}
+#endif
---
+++ 复用模块/ESP-NOW/ESPNOW.hpp
@@ -0,0 +1,167 @@
+/*
+ * @Description: 
+ * @Author: qingmeijiupiao
+ * @Date: 2024-05-19 14:44:23
+ */
+#ifndef esp_now_hpp
+#define esp_now_hpp
+#include <Arduino.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <esp_now.h>
+#include <map>
+#include <array>
+#include <vector>
+
+//发送数据包失败最大重试次数
+#define MAX_RETRY 5
+
+//数据包密钥
+constexpr uint16_t secret_key=0xFEFE;
+
+//回调函数
+using callback_func =std::function<void(data_package)>;
+
+//广播地址
+uint8_t broadcastMacAddress[] ={0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+
+//记录是否收到数据包，用于判断是否连接
+bool is_conect = false;
+
+/*↓↓↓↓声明↓↓↓↓*/
+
+//数据包
+struct HXC_ESPNOW_data_pakage;
+
+//ESP-NOW初始化
+void esp_now_setup(uint8_t* receive_MAC=broadcastMacAddress);
+
+//发送数据
+esp_err_t esp_now_send_package(String name,uint8_t* data,int datalen,uint8_t* receive_MAC=broadcastMacAddress);
+
+//添加回调函数
+void add_esp_now_callback(String package_name,callback_func func);
+
+//移除回调函数
+void remove_esp_now_callback(String package_name);
+
+
+
+
+/*↓↓↓↓定义↓↓↓↓*/
+
+//数据包格式
+struct HXC_ESPNOW_data_pakage {
+  uint16_t header_code=secret_key;//数据包头,作为密钥使用
+  uint8_t name_len;
+  uint8_t data_len;
+  String package_name;
+  uint8_t data[256];
+  //添加名字
+  void add_name(String _name){
+    package_name=_name;
+    name_len=package_name.length();
+  }
+  //添加数据
+  void add_data(uint8_t* _data,int _datalen){
+    data_len=_datalen;
+    for(int i=0;i<_datalen;i++){
+      data[i]=_data[i];
+    }
+  }
+  //获取数据
+  void get_data(uint8_t* _data){
+    _data[0]=header_code/256;
+    _data[1]=header_code%256;
+    _data[2]=package_name.length();
+    _data[3]=data_len;
+    memcpy(_data+4,package_name.c_str(),package_name.length());
+    memcpy(_data+4+package_name.length(),data,data_len);
+  }
+
+  //解码数组到结构体对象
+  void decode(uint8_t* _data,int _datalen){
+    header_code=_data[0]+_data[1]*256;
+    name_len=_data[2];
+    data_len=_data[3];
+    package_name="";
+    for(int i=0;i<name_len;i++){
+      package_name+=char(_data[4+i]);
+    }
+    for(int i=0;i<data_len;i++){
+      data[i]=_data[4+name_len+i];
+    }
+  }
+  //获取数据包长度
+  int get_len(){
+    return 4+name_len+data_len;
+  }
+
+};
+
+
+static std::map<String, callback_func> callback_map;//回调函数map
+
+void add_esp_now_callback(String package_name,callback_func func){
+  callback_map[package_name]=func;
+}
+void remove_esp_now_callback(String package_name){
+  callback_map.erase(package_name);
+};
+
+static HXC_ESPNOW_data_pakage re_data;//数据包缓存对象
+
+//接收数据时的回调函数，收到数据时自动运行
+void OnESPNOWDataRecv(const uint8_t *mac, const uint8_t *data, int len) {
+  //检查是否是数据包
+  if(len<2) return;
+  if(*(uint16_t*)data!=secret_key) return;
+  re_data.decode((uint8_t*)data,len);
+    //检查是否是需要运行回调函数的数据包
+  if(callback_map.count( re_data.package_name )!=0){
+    callback_map[re_data.package_name](re_data);
+  }
+  is_conect=true;
+}
+
+
+static esp_now_peer_info_t peerInfo;
+
+//ESP-NOW初始化
+void esp_now_setup(uint8_t* receive_MAC){
+
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != ESP_OK) {
+      Serial.println("ESP-NOW initialization failed");
+      return;
+  }
+
+  peerInfo.ifidx = WIFI_IF_STA;
+  memcpy(peerInfo.peer_addr, receive_MAC, 6);
+  esp_now_add_peer(&peerInfo);
+
+  if(receive_MAC!=broadcastMacAddress){
+    memcpy(peerInfo.peer_addr, broadcastMacAddress, 6);
+    esp_now_add_peer(&peerInfo);
+  }
+  esp_now_register_recv_cb(OnESPNOWDataRecv);
+} 
+
+
+//通过espnow发送数据包
+esp_err_t esp_now_send_package(String name,uint8_t* data,int datalen,uint8_t* receive_MAC=broadcastMacAddress){
+  HXC_ESPNOW_data_pakage send_data;
+  send_data.add_name(name);
+  send_data.add_data(data,datalen);
+  uint8_t send_data_array[send_data.get_len()];
+  send_data.get_data(send_data_array);
+  //发送
+  for(int i=0;i<MAX_RETRY;i++){
+    auto err = esp_now_send(receive_MAC,send_data_array,send_data.get_len());
+    if (err == ESP_OK)  return ESP_OK;
+    delay(20);
+  }
+  return ESP_FAIL;
+}
+
+#endif
